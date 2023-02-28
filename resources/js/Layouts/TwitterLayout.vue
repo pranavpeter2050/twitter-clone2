426c3657674957d2f2e2ebee66145e9cb365f166
pranav2050 <97501797+pranavpeter2050@users.noreply.github.com>
2023-02-28 17:42:51 +0530

    Added icons to base layout (TwitterLayout)

--- resources/js/Layouts/TwitterLayout.vue
+++ resources/js/Layouts/TwitterLayout.vue
@@ -1,9 +1,34 @@
 <script setup>
 import { ref } from 'vue';
 import { router } from '@inertiajs/vue3';
+import Twitter from 'vue-material-design-icons/Twitter.vue';
+import Magnify from 'vue-material-design-icons/Magnify.vue';
+import DotsHorizontal from 'vue-material-design-icons/DotsHorizontal.vue';
+import Feather from 'vue-material-design-icons/Feather.vue';
+import Close from 'vue-material-design-icons/Close.vue';
+import ChevronDown from 'vue-material-design-icons/ChevronDown.vue';
+import Earth from 'vue-material-design-icons/Earth.vue';
+import ImageOutline from 'vue-material-design-icons/ImageOutline.vue';
+import FileGifBox from 'vue-material-design-icons/FileGifBox.vue';
+import Emoticon from 'vue-material-design-icons/Emoticon.vue';
+import ArrowLeft from 'vue-material-design-icons/ArrowLeft.vue';
 
 </script>
 
 <template>
-
+    <div class="fixed w-full">
+        <div class="max-w-[1400px] flex mx-auto">
+            <div class="lg:w-3/12 w-[60px] h-[100vh] max-w-[350px] lg:px-4 lg:mx-auto">
+                <div class="p-2 px-3 mb-4">
+                    <Twitter fillColor="#FFFFFF" :size="37" />
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
+
+<style>
+body {
+    background-color: black;
+}
+</style>
